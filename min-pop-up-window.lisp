f7da89e575d5e7ba25363cbcb3d9a5b64b2d7284
Higashi, Hiromitsu <supercaveman@gmail.com>
2022-01-25 23:26:24 +0900

    add source-collection

---
+++ min-pop-up-window.lisp
@@ -0,0 +1,21 @@
+(ql:quickload :clx :silent t)		; => (:CLX)
+(defun pop-up-window (life-time &optional (host ""))
+  (let* ((display (xlib:open-display host))
+	 (screen (first (xlib:display-roots display)))
+	 (root-window (xlib:screen-root screen))
+	 (my-window (xlib:create-window
+		     :parent root-window
+		     :x 0
+		     :y 0
+		     :width 200
+		     :height 100)))
+    (xlib:map-window my-window)
+    (xlib:display-finish-output display)
+    (format t "should appear now~%")
+    (sleep life-time)
+    (xlib:destroy-window my-window)
+    (xlib:close-display display)))	; => POP-UP-WINDOW
+
+;; (pop-up-window 10)			; => should appear now
+;; NIL
+;; NIL
